3f75804e900cb9893fd946baff1763eae3cbb56c
aogg <happy2007xinlan@sina.com>
2020-11-14 14:39:57 +0800

    cloneByInsertSelect

--- src/Db.php
+++ src/Db.php
@@ -21,4 +21,50 @@ class Db extends \think\Db
         return $arr;
     }
 
+    /**
+     * insert根据select的数据
+     *
+     * @param string $fullTableName 完整表名
+     * @param string $whereString where条件
+     * @param array $fieldsValue 替换字段 ($filed=>$value)结构
+     * @return int
+     */
+    public function cloneByInsertSelect($fullTableName, $whereString, $fieldsValue = [])
+    {
+
+        /** @var Mysql $mysql */
+        $mysql = $this->instance();
+
+        $arr = $mysql->getTableFields($fullTableName);
+        $pk = $mysql->getPk($fullTableName);
+        $insertField = $selectField = '';
+
+        foreach ($arr as $field) {
+            // 去除主键
+            if (is_string($pk) && $pk === $field) {
+                continue;
+            }else if(is_array($pk) && in_array($field, $pk)){
+                continue;
+            }
+
+            // 构建
+            $insertField .= $field . ',';
+
+            if (isset($fieldsValue[$field])) {
+                $selectField .= $fieldsValue[$field] . ',';
+            }else{
+                $selectField .= $field . ',';
+            }
+        }
+        $insertField = rtrim($insertField, ',');
+        $selectField = rtrim($selectField, ',');
+
+        return static::execute(<<<SQL
+INSERT INTO `{$fullTableName}` ({$insertField})
+SELECT {$selectField} FROM `{$fullTableName}`
+WHERE {$whereString};
+SQL
+);
+    }
+
 }
